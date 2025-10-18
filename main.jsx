02fdfbc8314b9e89b95053c8ee1766e7a2533904
KSFToursLiverpool <kmaestheticsuk@icloud.com>
2025-10-18 23:02:09 +0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LiverpoolTaxiTours from './LiverpoolTaxiTours'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LiverpoolTaxiTours />
+  </React.StrictMode>,
+)
